5a797bbb458faf5b0a172f23616eb290b2291422
mamoru <tiusika.m@gamil.com>
2020-12-17 12:57:22 +0900

    Finish ch11

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.cache_classes = false
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -35,10 +35,14 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+
+  config.action_mailer.default_url_options = { host: 'example.com' }
+
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
